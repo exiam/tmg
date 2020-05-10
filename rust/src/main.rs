002220972edfc13b438a854d8521f585bec34380
Exiam <exiam@pm.me>
2020-05-11 00:00:55 +0200

    Add tag option

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,11 +1,11 @@
-use std::path::PathBuf;
-use dirs::home_dir;
 use chrono::prelude::*;
-use std::fs::{ File, OpenOptions, create_dir_all };
-use std::io::prelude::*;
-use termion::{ color, style };
+use dirs::home_dir;
 use std::collections::HashMap;
+use std::fs::{create_dir_all, File, OpenOptions};
+use std::io::prelude::*;
+use std::path::PathBuf;
 use structopt::StructOpt;
+use termion::{color, style};
 
 #[derive(StructOpt, Debug)]
 struct Cli {
@@ -14,7 +14,7 @@ struct Cli {
     debug: bool,
     /// Subcommand
     #[structopt(subcommand)]
-    cmd: Command
+    cmd: Command,
 }
 
 #[derive(StructOpt, Debug)]
@@ -27,6 +27,10 @@ enum Command {
         /// Option to apply specific time
         #[structopt(short, long, parse(try_from_str = parse_time))]
         at: Option<String>,
+
+        /// Option to apply specific tag
+        #[structopt(short, long)]
+        tag: Option<String>,
     },
     /// Stop a task
     Stop {
@@ -37,16 +41,16 @@ enum Command {
         #[structopt(short, long, parse(try_from_str = parse_time))]
         at: Option<String>,
 
-        /// Activate debug mode
+        /// Option to apply specific tag
         #[structopt(short, long)]
-        debug: bool
+        tag: Option<String>,
     },
     /// View all tasks
     View {
         /// Specific month to display
         #[structopt(short, long)]
         at: Option<String>,
-    }
+    },
 }
 
 fn main() {
@@ -62,18 +66,24 @@ fn main() {
     let mut command_options = HashMap::new();
 
     match args.cmd {
-        Command::Start { task, at, .. } => {
+        Command::Start { task, at, tag, .. } => {
             if let Some(_) = at {
                 command_options.insert(String::from("at"), at.unwrap());
+            }
+            if let Some(_) = tag {
+                command_options.insert(String::from("tag"), tag.unwrap());
             }
             write_command(String::from("start"), task, command_options);
-        },
-        Command::Stop { task, at, .. } => {
+        }
+        Command::Stop { task, at, tag, .. } => {
             if let Some(_) = at {
                 command_options.insert(String::from("at"), at.unwrap());
             }
+            if let Some(_) = tag {
+                command_options.insert(String::from("tag"), tag.unwrap());
+            }
             write_command(String::from("stop"), task, command_options);
-        },
+        }
         Command::View { at } => {
             view_file_command(at);
         }
@@ -83,7 +93,7 @@ fn main() {
 fn parse_time(value: &str) -> Result<String, &str> {
     match NaiveTime::parse_from_str(value, "%H:%M") {
         Ok(_) => Ok(String::from(value)),
-        Err(_) => Err("Cannot parse time. Should use format HH:MM.")
+        Err(_) => Err("Cannot parse time. Should use format HH:MM."),
     }
 }
 
@@ -91,10 +101,8 @@ fn write_command(action: String, task: String, options: HashMap<String, String>)
     let date: DateTime<Local>;
 
     if options.contains_key("at") {
-        let time = NaiveTime::parse_from_str(
-            options.get("at").unwrap(),
-            "%H:%M"
-        ).expect("Cannot parse time");
+        let time = NaiveTime::parse_from_str(options.get("at").unwrap(), "%H:%M")
+            .expect("Cannot parse time");
 
         let naive_date = NaiveDateTime::new(Local::today().naive_utc(), time);
         date = Local.from_local_datetime(&naive_date).unwrap();
@@ -102,13 +110,13 @@ fn write_command(action: String, task: String, options: HashMap<String, String>)
         date = Local::now();
     }
 
-    let (
-        year, 
-        month, 
-        day,
-        hour,
-        minute
-    ) = (date.year(), date.month(), date.day(), date.hour(), date.minute());
+    let (year, month, day, hour, minute) = (
+        date.year(),
+        date.month(),
+        date.day(),
+        date.hour(),
+        date.minute(),
+    );
 
     // Get report file
     let mut report_file = OpenOptions::new()
@@ -118,22 +126,36 @@ fn write_command(action: String, task: String, options: HashMap<String, String>)
         .open(get_report_path(year, month))
         .unwrap(); // Avoid match
 
-    let formatted_string = format!(
-        "{}-{:0>2}-{:0>2}@{:0>2}:{:0>2} -> {} ({})", 
-        year, 
-        month,
-        day,
-        hour,
-        minute,
-        task,
-        action
-    );
-    
+    let formatted_string;
+
+    if options.contains_key("tag") {
+        formatted_string = format!(
+            "{}-{:0>2}-{:0>2}@{:0>2}:{:0>2} -> {} ({}) #{}",
+            year,
+            month,
+            day,
+            hour,
+            minute,
+            task,
+            action,
+            options.get("tag").unwrap()
+        );
+    } else {
+        formatted_string = format!(
+            "{}-{:0>2}-{:0>2}@{:0>2}:{:0>2} -> {} ({})",
+            year, month, day, hour, minute, task, action
+        );
+    }
     if let Err(e) = writeln!(report_file, "{}", formatted_string) {
         eprintln!("Cannot write in file: {}", e);
     }
 
-    println!("{}Write:{} {}", color::Fg(color::Green), style::Reset, formatted_string);
+    println!(
+        "{}Write:{} {}",
+        color::Fg(color::Green),
+        style::Reset,
+        formatted_string
+    );
 }
 
 fn view_file_command(at: Option<String>) {
@@ -142,28 +164,26 @@ fn view_file_command(at: Option<String>) {
     if let Some(_) = at {
         let mut date_string = at.unwrap();
         date_string.push_str("-01");
-        let naive_date = NaiveDate::parse_from_str(&date_string, "%Y-%m-%d").expect("Cannot parse date");
+        let naive_date =
+            NaiveDate::parse_from_str(&date_string, "%Y-%m-%d").expect("Cannot parse date");
         local = Local.from_local_date(&naive_date).unwrap();
     } else {
         local = Local::today();
     }
 
-    let (
-        year, 
-        month
-    ) = (local.year(), local.month());
+    let (year, month) = (local.year(), local.month());
 
     let report_path = get_report_path(year, month);
     let path_clone = report_path.clone();
-    
     let file_name = path_clone.file_name().expect("Cannot retrieve file name");
 
     let mut file = File::open(report_path).expect("Cannot open file");
     let mut file_content = String::new();
-    file.read_to_string(&mut file_content).expect("Cannot read file");
+    file.read_to_string(&mut file_content)
+        .expect("Cannot read file");
 
     println!(
-        "{}View file '{}':{}", 
+        "{}View file '{}':{}",
         color::Fg(color::Green),
         file_name.to_string_lossy(),
         style::Reset
@@ -178,7 +198,7 @@ fn view_file_command(at: Option<String>) {
 fn get_app_dir() -> PathBuf {
     let mut dir = match home_dir() {
         Some(path) => PathBuf::from(path),
-        None => PathBuf::from("")
+        None => PathBuf::from(""),
     };
 
     dir.push(".tmg");
